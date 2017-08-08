bef534df88d9980c5be3b5864e7df9279e04c67f
KorGgenT <curtis.r.merrill@gmail.com>
2017-08-08 14:30:57 -0400

    added creatio recipes for ores

--- TM-addon-omni/angelsrefining/recipes.lua
+++ TM-addon-omni/angelsrefining/recipes.lua
@@ -4,3 +4,15 @@ TM.item_add_aspect("angels-ore5", "Examinis", 50) --rubyte
 TM.item_add_aspect("angels-ore6", "Vitium", 50) --bobmonium
 TM.item_add_aspect("angels-ore2", "Fabrico", 50) --jivolite
 TM.item_add_aspect("angels-ore4", "Aversio", 50) --crotinnium
+
+if creatio_enabled then
+	local creatio_ores = {
+		{"angels-ore1", 0, 0, 0},
+		{"angels-ore2", 0, 0, 0},
+		{"angels-ore3", 0, 0, 0},
+		{"angels-ore4", 0, 0, 0},
+		{"angels-ore5", 0, 0, 0},
+		{"angels-ore6", 0, 0, 0},
+	}
+	TM.creatio_pairs(creatio_ores)
+end
--- TM-addon-omni/bobores/recipes.lua
+++ TM-addon-omni/bobores/recipes.lua
@@ -8,3 +8,19 @@ TM.item_add_aspect("silver-ore", "Auram", 50)
 TM.item_add_aspect("cobalt-ore", "Vinculum", 50)
 TM.item_add_aspect("tungsten-ore", "Asidia", 50)
 TM.item_add_aspect("titanium-ore", "Praemundio", 50)
+
+if creatio_enabled then
+	local creatio_ores = {
+		{"lead-ore", 0, 0, 0},
+		{"tin-ore", 0, 0, 0},
+		{"nickel-ore", 0, 0, 0},
+		{"quartz-ore", 0, 0, 0},
+		{"zinc-ore", 0, 0, 0},
+		{"gold-ore", 0, 0, 0},
+		{"silver-ore", 0, 0, 0},
+		{"cobalt-ore", 0, 0, 0},
+		{"tungsten-ore", 0, 0, 0},
+		{"titanium-ore", 0, 0, 0},
+	}
+	TM.creatio_pairs(creatio_ores)
+end
